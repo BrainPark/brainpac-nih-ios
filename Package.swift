64370417a1db97a654e53de8aa3230861ef2e131
Faisha Surjatin <faisha.surjatin@monash.edu>
2023-04-27 14:54:28 +1000

    Add binary target to package

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "brainpac-nih-ios",
-            targets: []),
+            targets: ["UnityFramework"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,5 +18,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
+            name: "UnityFramework",
+            url: "https://api.github.com/repos/BrainPark/brainpac-nih-ios/releases/assets/105518582.zip",
+            checksum: "a08d82d677796569ade2d9ab5d9d319e170215ca34c1af4611dd000dd7a93c65"
+        ),
     ]
 )
